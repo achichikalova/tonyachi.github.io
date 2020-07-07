032a79521533b5bb2645560bfdafd7262265e19b
a.chichikalova <tonia.chichikalova@gmail.com>
2020-07-07 17:13:35 -0400

    added slider for modal

--- js/app.js
+++ js/app.js
@@ -4,12 +4,56 @@ $(document).ready(function () {
     $(".nav-btn").click(function () {
         var cat = $(this).attr("data-filter");
         if (cat == "all") {
-            $(".portfolio__item").show("1000");
+            $(".portfolio__item").show();
         } else {
-            $(".portfolio__item").not("." + cat).hide("1000");
-            $(".portfolio__item").filter("." + cat).show("1000");
+            $(".portfolio__item").not("." + cat).hide();
+            $(".portfolio__item").filter("." + cat).show();
         }
     });
+
+
+    /* Modals */
+    const modalCall = $("[data-modal]");
+    const modalClose = $(".modal__close");
+
+    modalCall.click(function () {
+
+        let modalId = $(this).data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+        $("#worksSlider").slick('setPosition');
+    });
+
+    modalClose.click(function () {
+
+        let modalParent = $(this).parents('.modal');
+
+        modalParent.removeClass('show');
+        $("body").removeClass('no-scroll');
+    });
+
+    $(".modal").click(function () {
+        $(this).removeClass('show');
+        $("body").removeClass('no-scroll');
+    });
+
+    $(".modal__dialog").click(function (event) {
+        event.stopPropagation();
+    });
+
+    /* Slider: https://kenwheeler.github.io/slick/ */
+    $('#worksSlider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: false
+    });
+
+
+
 
 
 
